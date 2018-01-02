54d4313f1f9984f3ab196862100f5b5e0893041d
PeterLinX <18610565179@163.com>
2018-01-02 20:48:44 +0800

    init

---
+++ NeoDataTransformation/Helper.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NeoDataTransformation
+{
+    public static class Helper
+    {
+        private static ThreadLocal<SHA256> _sha256 = new ThreadLocal<SHA256>(() => SHA256.Create());
+        private static ThreadLocal<RIPEMD160Managed> _ripemd160 = new ThreadLocal<RIPEMD160Managed>(() => new RIPEMD160Managed());
+
+        public static byte AddressVersion = 23;
+
+        public static string ToHexString(this IEnumerable<byte> value)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in value)
+                sb.AppendFormat("{0:x2}", b);
+            return sb.ToString();
+        }
+
+        public static byte[] HexToBytes(this string value)
+        {
+            if (value == null || value.Length == 0)
+                return new byte[0];
+            if (value.Length % 2 == 1)
+                throw new FormatException();
+            byte[] result = new byte[value.Length / 2];
+            for (int i = 0; i < result.Length; i++)
+                result[i] = byte.Parse(value.Substring(i * 2, 2), NumberStyles.AllowHexSpecifier);
+            return result;
+        }
+
+
+        public static UInt160 ToScriptHash(this byte[] script)
+        {
+            return new UInt160(Helper.Hash160(script));
+        }
+
+        public static byte[] Hash160(byte[] message)
+        {
+            return message.Sha256().RIPEMD160();
+        }
+
+        public static byte[] Sha256(this IEnumerable<byte> value)
+        {
+            return _sha256.Value.ComputeHash(value.ToArray());
+        }
+
+        public static byte[] Sha256(this byte[] value, int offset, int count)
+        {
+            return _sha256.Value.ComputeHash(value, offset, count);
+        }
+
+        public static byte[] RIPEMD160(this IEnumerable<byte> value)
+        {
+            return _ripemd160.Value.ComputeHash(value.ToArray());
+        }
+
+        public static UInt160 ToScriptHash(string address)
+        {
+            byte[] data = address.Base58CheckDecode();
+            if (data.Length != 21)
+                throw new FormatException();
+            if (data[0] != Helper.AddressVersion)
+                throw new FormatException();
+            return new UInt160(data.Skip(1).ToArray());
+        }
+
+        public static string ToAddress(UInt160 scriptHash)
+        {
+            byte[] data = new byte[21];
+            data[0] = Helper.AddressVersion;
+            Buffer.BlockCopy(scriptHash.ToArray(), 0, data, 1, 20);
+            return data.Base58CheckEncode();
+        }
+
+        public static byte[] Base58CheckDecode(this string input)
+        {
+            byte[] buffer = Base58.Decode(input);
+            if (buffer.Length < 4) throw new FormatException();
+            byte[] checksum = buffer.Sha256(0, buffer.Length - 4).Sha256();
+            if (!buffer.Skip(buffer.Length - 4).SequenceEqual(checksum.Take(4)))
+                throw new FormatException();
+            return buffer.Take(buffer.Length - 4).ToArray();
+        }
+
+        public static string Base58CheckEncode(this byte[] data)
+        {
+            byte[] checksum = data.Sha256().Sha256();
+            byte[] buffer = new byte[data.Length + 4];
+            Buffer.BlockCopy(data, 0, buffer, 0, data.Length);
+            Buffer.BlockCopy(checksum, 0, buffer, data.Length, 4);
+            return Base58.Encode(buffer);
+        }
+    }
+
+}
---
+++ NeoDataTransformation/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace NeoDataTransformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //字符："transfer" = "7472616e73666572"
+            string str1 = "transfer";
+            byte[] byteArray = Encoding.UTF8.GetBytes(str1);
+            string hexStr = byteArray.ToHexString();
+            Console.WriteLine("hexStr: " + hexStr);
+
+            //字符："7472616e73666572" = "transfer"
+            string str2 = "7472616e73666572";
+            byte[] tmp2 = str2.HexToBytes();
+            string keyStr = Encoding.ASCII.GetString(tmp2);
+            Console.WriteLine("normalStr: " + keyStr);
+
+            //数字：100000000 = 00e1f505
+            string str3 = "100000000";
+            BigInteger bigInteger = BigInteger.Parse(str3);
+            string hexNumber = bigInteger.ToByteArray().ToHexString();
+            Console.WriteLine("hexStr: " + hexNumber);
+
+            //数字：00e1f505 = 100000000
+            string str4 = "00e1f505";
+            byte[] tmp1 = str4.HexToBytes();
+            BigInteger biResult = new BigInteger(tmp1);
+            Console.WriteLine("normalStr: " + biResult);
+
+            //地址： AeV59NyZtgj5AMQ7vY6yhr2MRvcfFeLWSb = ecc6b20d3ccac1ee9ef109af5a7cdb85706b1df9
+            string str5 = "AeV59NyZtgj5AMQ7vY6yhr2MRvcfFeLWSb";
+            UInt160 address = Helper.ToScriptHash(str5);
+            Console.WriteLine(address);
+
+            //地址： ecc6b20d3ccac1ee9ef109af5a7cdb85706b1df9 = AeV59NyZtgj5AMQ7vY6yhr2MRvcfFeLWSb
+            string str6 = "ecc6b20d3ccac1ee9ef109af5a7cdb85706b1df9";
+            string strFromScriptHash = Helper.ToAddress(UInt160.Parse(str6));
+            Console.WriteLine(strFromScriptHash);
+
+
+            Console.ReadLine();
+        }
+    }
+}
